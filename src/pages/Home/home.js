2705e36c0e09ad9b0933ff61dcfeddbb72075a8f
duna <dunaalessa@gmail.com>
2024-05-08 12:13:25 +0300

    exploreMenu

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './home.css'
 import Header from '../../componets/Header/Header'
+import ExprolMenu from '../../componets/ExploreMenu/ExploreMenu'
 
 const home = () => {
   return (
     <div>
       <Header/>
+      <ExprolMenu/>
     </div>
   )
 }
